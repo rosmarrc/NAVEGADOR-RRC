066801bc36177003766e9baa02ba2bede6b88f24
Rosmar Rijo Castro <79125757+rosmarrc@users.noreply.github.com>
2023-03-18 22:14:11 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace navegador_rrc2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_inicio = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_ir = new System.Windows.Forms.Button();
+            this.btn_refrescar = new System.Windows.Forms.Button();
+            this.btn_adelante = new System.Windows.Forms.Button();
+            this.btn_retroceder = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_inicio);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btn_ir);
+            this.panel1.Controls.Add(this.btn_refrescar);
+            this.panel1.Controls.Add(this.btn_adelante);
+            this.panel1.Controls.Add(this.btn_retroceder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 57);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_inicio
+            // 
+            this.btn_inicio.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_inicio.BackColor = System.Drawing.Color.LightGray;
+            this.btn_inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_inicio.Font = new System.Drawing.Font("Berlin Sans FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_inicio.Location = new System.Drawing.Point(644, 0);
+            this.btn_inicio.Name = "btn_inicio";
+            this.btn_inicio.Size = new System.Drawing.Size(75, 57);
+            this.btn_inicio.TabIndex = 5;
+            this.btn_inicio.Text = "Inicio";
+            this.btn_inicio.UseVisualStyleBackColor = false;
+            this.btn_inicio.Click += new System.EventHandler(this.btn_inicio_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(162, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btn_ir
+            // 
+            this.btn_ir.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_ir.Location = new System.Drawing.Point(579, 20);
+            this.btn_ir.Name = "btn_ir";
+            this.btn_ir.Size = new System.Drawing.Size(49, 26);
+            this.btn_ir.TabIndex = 3;
+            this.btn_ir.Text = "Ir";
+            this.btn_ir.UseVisualStyleBackColor = true;
+            this.btn_ir.Click += new System.EventHandler(this.btn_ir_Click);
+            // 
+            // btn_refrescar
+            // 
+            this.btn_refrescar.BackColor = System.Drawing.Color.LightGray;
+            this.btn_refrescar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_refrescar.Font = new System.Drawing.Font("Berlin Sans FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_refrescar.Location = new System.Drawing.Point(54, 0);
+            this.btn_refrescar.Name = "btn_refrescar";
+            this.btn_refrescar.Size = new System.Drawing.Size(102, 57);
+            this.btn_refrescar.TabIndex = 2;
+            this.btn_refrescar.Text = "Refrescar";
+            this.btn_refrescar.UseVisualStyleBackColor = false;
+            this.btn_refrescar.Click += new System.EventHandler(this.btn_refrescar_Click);
+            // 
+            // btn_adelante
+            // 
+            this.btn_adelante.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_adelante.BackColor = System.Drawing.Color.LightGray;
+            this.btn_adelante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_adelante.Font = new System.Drawing.Font("Wide Latin", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_adelante.Location = new System.Drawing.Point(725, 0);
+            this.btn_adelante.Name = "btn_adelante";
+            this.btn_adelante.Size = new System.Drawing.Size(75, 57);
+            this.btn_adelante.TabIndex = 1;
+            this.btn_adelante.Text = "-->";
+            this.btn_adelante.UseVisualStyleBackColor = false;
+            this.btn_adelante.Click += new System.EventHandler(this.btn_adelante_Click);
+            // 
+            // btn_retroceder
+            // 
+            this.btn_retroceder.BackColor = System.Drawing.Color.LightGray;
+            this.btn_retroceder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btn_retroceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_retroceder.Font = new System.Drawing.Font("Wide Latin", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_retroceder.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_retroceder.Location = new System.Drawing.Point(0, 0);
+            this.btn_retroceder.Name = "btn_retroceder";
+            this.btn_retroceder.Size = new System.Drawing.Size(48, 57);
+            this.btn_retroceder.TabIndex = 0;
+            this.btn_retroceder.Text = "<--";
+            this.btn_retroceder.UseVisualStyleBackColor = false;
+            this.btn_retroceder.Click += new System.EventHandler(this.btn_retroceder_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 57);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 393);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.VisibleChanged += new System.EventHandler(this.btn_inicio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Navegador RRC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_ir;
+        private System.Windows.Forms.Button btn_refrescar;
+        private System.Windows.Forms.Button btn_adelante;
+        private System.Windows.Forms.Button btn_retroceder;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btn_inicio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace navegador_rrc2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ir_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void btn_retroceder_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btn_refrescar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void btn_adelante_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btn_inicio_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+            textBox1.Clear();
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            { btn_ir_Click(sender, e); }
+        }
+    }
+}
